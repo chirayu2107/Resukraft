85e1ab37b38ec49952ac235e13737ec4eca8d31c
Chirayu Maru <144110269+chirayu2107@users.noreply.github.com>
2024-11-22 17:29:47 +0530

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,13 +5,13 @@ import Viewer from "./viewer";
 
 function App() {
   return (
-    <div font-sans mx="2 md:5" xl:mx-auto xl:px-5 class="max-w-[18px]">
+    <div font-sans mx="2 md:5" xl:mx-auto xl:px-5 class="max-w-[1820px]">
       <Navbar />
       <ModesBtn />
 
       {/* use this for html-to-image (svg) */}
       <div id="svgWrapper" rounded-none md:grid justify-center>
-        {editorMode() ? <Editor /> : <Viewer />}
+        {editorMode() ? <Editor /> : <View />}
       </div>
     </div>
   );
