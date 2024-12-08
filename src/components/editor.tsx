ee3306c091c4e707f181b3dd1626cd3284f53541
Chirayu Maru <144110269+chirayu2107@users.noreply.github.com>
2024-12-08 18:25:38 +0530

    Update editor.tsx

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,282 +1,235 @@
-import { store } from "../store";
+import { setStore, store } from "../store";
+import { For } from "solid-js";
 import { Icon } from "@iconify-icon/solid";
-import { Document, Page, Text, View, StyleSheet } from "@react-pdf/core";
 
-// Define PDF styles
-const styles = StyleSheet.create({
-  page: {
-    flexDirection: "column",
-    backgroundColor: "#fff",
-    padding: 30,
-  },
-  section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1,
-  },
-  name: {
-    fontSize: 24,
-    textAlign: "center",
-    fontFamily: "Raleway",
-  },
-  designation: {
-    fontSize: 18,
-    textAlign: "center",
-    fontFamily: "Raleway",
-    marginBottom: 10,
-  },
-  sectionTitle: {
-    fontSize: 16,
-    fontWeight: "bold",
-    marginBottom: 5,
-  },
-  sectionContent: {
-    fontSize: 14,
-    fontFamily: "Helvetica",
-  },
-  sectionItem: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginBottom: 5,
-  },
-  itemTitle: {
-    fontSize: 14,
-    fontWeight: "bold",
-    textDecoration: "underline",
-  },
-  itemDetails: {
-    fontSize: 14,
-    backgroundColor: "#f0f0f0",
-    padding: 5,
-    borderRadius: 5,
-  },
-  skillsContainer: {
-    flexWrap: "wrap",
-    flexDirection: "row",
-  },
-  skillItem: {
-    backgroundColor: "#f0f0f0",
-    padding: 5,
-    borderRadius: 5,
-    marginRight: 5,
-    marginBottom: 5,
-    fontSize: 14,
-  },
-});
-
-const ResumeView = () => {
-  // Function to generate PDF
-  const generatePDF = () => {
-    window.print();
-  };
-
+const PersonalDetails = () => {
+  const placeholders = [
+    { name: "full name" },
+    { name: "designation", altname: "designation (ex: Web dev)" },
+    { name: "phone" },
+    { name: "address" },
+    { name: "email address" },
+    { name: "github username" },
+  ];
   return (
-    <Document>
-      <Page size="A4" style={styles.page}>
-        {/* Header */}
-        <View style={styles.section}>
-          <Text style={styles.name}>
-            {store["full name"].split(" ")[0]}{" "}
-            <Text style={{ fontWeight: "normal", color: "#0070f3" }}>
-              {store["full name"].split(" ")[1]}
-            </Text>
-          </Text>
-          <Text style={styles.designation}>{store.designation}</Text>
-        </View>
-
-        {/* Career Objective */}
-        <View style={styles.section}>
-          <Text style={styles.sectionTitle}>
-            <Icon
-              icon="mdi:target-arrow"
-              width="20"
-              style={{
-                backgroundColor: "#fef2f2",
-                color: "#dc2626",
-                padding: 5,
-                borderRadius: 20,
-                marginRight: 5,
-              }}
-            />
-            About
-          </Text>
-          <Text style={styles.sectionContent}>{store.profile}</Text>
-        </View>
-
-        {/* Education */}
-        <View style={styles.section}>
-          <Text style={styles.sectionTitle}>
-            <Icon
-              icon="mdi:education-outline"
-              width="20"
-              style={{
-                backgroundColor: "#f0f9ff",
-                color: "#0369a1",
-                padding: 5,
-                borderRadius: 20,
-                marginRight: 5,
-              }}
-            />
-            Education
-          </Text>
-          {store.education.map((item, index) => (
-            <View key={index} style={styles.sectionItem}>
-              <Text style={styles.itemTitle}>{item.institution}</Text>
-              <Text style={styles.itemDetails}>{item.time}</Text>
-            </View>
-          ))}
-        </View>
-
-        {/* Work Experience */}
-        <View style={styles.section}>
-          <Text style={styles.sectionTitle}>
-            <Icon
-              icon="uil:suitcase"
-              width="20"
-              style={{
-                backgroundColor: "#f3e8ff",
-                color: "#6d28d9",
-                padding: 5,
-                borderRadius: 20,
-                marginRight: 5,
-              }}
-            />
-            Work Experience
-          </Text>
-          {store["work experience"].map((item, index) => (
-            <View key={index} style={styles.sectionItem}>
-              <Text style={styles.itemTitle}>{item.company}</Text>
-              <Text style={styles.itemDetails}>{item.time}</Text>
-            </View>
-          ))}
-        </View>
-
-        {/* Personal Details */}
-        <View style={styles.section}>
-          <Text style={styles.sectionTitle}>
-            <Icon
-              icon="mdi:user-circle"
-              width="20"
-              style={{
-                backgroundColor: "#e9d5ff",
-                color: "#6366f1",
-                padding: 5,
-                borderRadius: 20,
-                marginRight: 5,
-              }}
-            />
-            Personal Details
-          </Text>
-          <View>
-            {store.address && (
-              <View style={styles.sectionItem}>
-                <Text style={styles.itemTitle}>Address</Text>
-                <Text style={styles.itemDetails}>{store.address}</Text>
-              </View>
-            )}
-            <View style={styles.sectionItem}>
-              <Text style={styles.itemTitle}>Phone</Text>
-              <Text style={styles.itemDetails}>{store.phone}</Text>
-            </View>
-            <View style={styles.sectionItem}>
-              <Text style={styles.itemTitle}>Email</Text>
-              <Text style={styles.itemDetails}>{store["email address"]}</Text>
-            </View>
-            <View style={styles.sectionItem}>
-              <Text style={styles.itemTitle}>Github</Text>
-              <Text style={styles.itemDetails}>@{store["github username"]}</Text>
-            </View>
-          </View>
-        </View>
-
-        {/* Skills and Interests */}
-        <View style={[styles.section, styles.skillsContainer]}>
-          <View style={{ flex: 1 }}>
-            <Text style={styles.sectionTitle}>
-              <Icon
-                icon="ph-code-bold"
-                width="20"
-                style={{
-                  backgroundColor: "#fff7ed",
-                  color: "#f97316",
-                  padding: 5,
-                  borderRadius: 20,
-                  marginRight: 5,
-                }}
-              />
-              Technical Skills
-            </Text>
-            {store.skills.map((skill, index) => (
-              <Text key={index} style={styles.skillItem}>
-                {skill}
-              </Text>
-            ))}
-          </View>
-          <View style={{ flex: 1 }}>
-            <Text style={styles.sectionTitle}>
-              <Icon
-                icon="typcn:point-of-interest"
-                width="20"
-                style={{
-                  backgroundColor: "#f0f0f0",
-                  color: "#0070f3",
-                  padding: 5,
-                  borderRadius: 20,
-                  marginRight: 5,
-                }}
-              />
-              Interests & Hobbies
-            </Text>
-            {store.interests.map((interest, index) => (
-              <Text key={index} style={styles.skillItem}>
-                {interest}
-              </Text>
-            ))}
-          </View>
-        </View>
-
-        {/* Open Source Projects */}
-        <View style={styles.section}>
-          <Text style={styles.sectionTitle}>
-            <Icon
-              icon="tabler:brand-open-source"
-              width="20"
-              style={{
-                backgroundColor: "#d1fae5",
-                color: "#16a34a",
-                padding: 5,
-                borderRadius: 20,
-                marginRight: 5,
-              }}
-            />
-            Open Source Projects
-          </Text>
-          {store.oss.map((project, index) => (
-            <View key={index} style={styles.sectionItem}>
-              <Text style={styles.itemTitle}>{project["project name"]}</Text>
-              <Text style={styles.itemDetails}>({project.languages})</Text>
-            </View>
-          ))}
-        </View>
-
-        {/* Download Button */}
-        <View style={{ flexDirection: "row", justifyContent: "flex-end" }}>
-          <Text
-            style={{
-              backgroundColor: "#0070f3",
-              color: "#fff",
-              padding: 10,
-              borderRadius: 5,
-              cursor: "pointer",
-            }}
-            onPress={generatePDF}
-          >
-            Download PDF
-          </Text>
-        </View>
-      </Page>
-    </Document>
+    <div>
+      <h3>Personal Details</h3>
+      <div grid md:grid-cols-2 gap-3>
+        {placeholders.map((x: any) => (
+          <input
+            placeholder={x.altname || x.name}
+            onChange={(e) => setStore(x.name, e.target.value)}
+            value={store[x.name] || ""}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+const About = () => {
+  const placeholders = [
+    {
+      name: "profile",
+      altname: "profile (write short self introduction & goals)",
+    },
+  ];
+  return (
+    <div>
+      <h3>About</h3>
+      <div grid gap-3>
+        {placeholders.map((x: any) => (
+          <input
+            placeholder={x.altname || x.name}
+            onChange={(e) => setStore(x.name, e.target.value)}
+            value={store[x.name] || ""}
+          />
+        ))}
+      </div>
+    </div>
   );
 };
 
-export default ResumeView;
+const Education_WorkExp = (props: any) => {
+  const { store_name } = props;
+
+  return (
+    <div>
+      <h3 capitalize>{store_name}</h3>
+      <div grid gap-3>
+        <For each={store[store_name]}>
+          {(obj, index) => (
+            <div flex flex-col md:flex-row gap-3 items-center>
+              {Object.entries(obj).map(([key, value]) => (
+                <input
+                  w-full
+                  placeholder={key}
+                  value={value}
+                  onchange={(e) =>
+                    setStore(store_name, index(), key, e.target.value)}
+                />
+              ))}
+              <button
+                aria-label="delete input"
+                class="!bg-slate-7 !p-1 text-3xl !rounded-full"
+                onclick={() => {
+                  setStore(
+                    store_name,
+                    store[store_name].filter((_, i) => i !== index()),
+                  );
+                }}
+              >
+                <Icon
+                  icon="ion:close-circle"
+                  class="text-red-3 dark:text-red-4 hover:text-red-2"
+                />
+              </button>
+            </div>
+          )}
+        </For>
+      </div>
+
+      <button
+        class="!bg-blue-6 mt-5 mx-auto !text-white-1"
+        onclick={() => {
+          let emptyObj = store_name == "education"
+            ? { institution: "", qualification: "", time: "" }
+            : { company: "", position: "", time: "" };
+
+          setStore(store_name, [...store[store_name], emptyObj]);
+        }}
+      >
+        <Icon icon="ic:round-add-circle" text-xl />
+        Add
+      </button>
+    </div>
+  );
+};
+
+const Skills_Hobbies = (props: any) => {
+  const { store_name } = props;
+
+  return (
+    <div>
+      <h3 capitalize>{store_name}</h3>
+
+      <div grid md:flex flex-wrap gap-3>
+        <For each={store[store_name]}>
+          {(val, index) => (
+            <div
+              bg="slate-1 dark:black-3"
+              grid
+              grid-cols="[1fr_auto]"
+              items-center
+              focus-within="border-2 border-solid border-blue"
+              rounded-lg
+            >
+              <input
+                rounded-r-none
+                w-full
+                focus:outline-none
+                placeholder={store_name == "skills"
+                  ? "skill"
+                  : "interest / hobby"}
+                value={val}
+                size={val.length}
+                onchange={(e) =>
+                  setStore(store_name, index(), e.target.value)}
+              />
+              <button
+                aria-label="delete input"
+                class="shadow-none !bg-slate-1 dark:!bg-black-3 p-2 text-3xl rounded-l-none"
+                onclick={() =>
+                  setStore(
+                    store_name,
+                    store[store_name].filter((_, i) => i !== index()),
+                  )}
+              >
+                <Icon
+                  icon="ion:close-circle"
+                  class="text-red-4 dark:text-red-4 hover:text-red-2"
+                />
+              </button>
+            </div>
+          )}
+        </For>
+        <button
+          class="!bg-blue-6 !text-white-1"
+          onclick={() => {
+            setStore(store_name, [...store[store_name], ""]);
+          }}
+        >
+          <Icon icon="ic:round-add-circle" text-xl />
+          Add
+        </button>
+      </div>
+    </div>
+  );
+};
+const Oss = () => (
+  <div>
+    <h3 capitalize>Open source projects</h3>
+    {/* input fields */}
+    <div grid gap-3>
+      <For each={store.oss}>
+        {(obj, index) => (
+          <div flex flex-col md:flex-row gap-3 items-center>
+            {Object.entries(obj).map(([key, value]) => (
+              <input
+                w-full
+                placeholder={key}
+                value={value}
+                onchange={(e) => setStore("oss", index(), key, e.target.value)}
+              />
+            ))}
+            <button
+              aria-label="delete input"
+              class="!bg-slate-7 !p-1 text-3xl !rounded-full"
+              onclick={() =>
+                setStore(
+                  "oss",
+                  store.oss.filter((_, i) => i !== index()),
+                )}
+            >
+              <Icon
+                icon="ion:close-circle"
+                class="text-red-3 dark:text-red-4 hover:text-red-2"
+              />
+            </button>
+          </div>
+        )}
+      </For>
+    </div>
+
+    <button
+      class="!bg-blue-6 mt-5 mx-auto !text-white-1"
+      onclick={() => {
+        setStore("oss", [...store.oss, {
+          "project name": "",
+          description: "",
+          languages: "",
+        }]);
+      }}
+    >
+      <Icon icon="ic:round-add-circle" text-xl />
+      Add
+    </button>
+  </div>
+);
+
+export default () => (
+  <div
+    bg="white-1 dark:black-2"
+    p="5 md:10"
+    pt="0 md:5"
+    class="max-w-5xl center shadow-sm rounded-xl grid gap-5 [&_*]:animate_smooth"
+    animate_smooth
+  >
+    <PersonalDetails />
+    <About />
+    <Education_WorkExp store_name="education" />
+    <Education_WorkExp store_name="work experience" />
+    <Skills_Hobbies store_name="skills" />
+    <Skills_Hobbies store_name="interests" />
+    <Oss />
+  </div>
+);
